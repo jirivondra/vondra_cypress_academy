6ebd57134c532eda5cb63c802eedb4038a1b782f
Jiří Vondra <voji1@yahoo.com>
2025-09-10 19:27:27 +0200

    ddt

---
+++ cypress/e2e/learning/ddt/ddt_crete_project_tests.cy.js
@@ -0,0 +1,16 @@
+// ddt_create_project_tests.cy.js
+// cypress/e2e/learning/ddt
+
+/* 
+* TODO vytváření ddt
+0. Vytvoření a úprava PageObjectů pro vytvoření a kontrolu projektu
+1. Vytvoření JSON souboru s daty pro testy
+2. V testu importovat JSON
+3. Vytvoření cyklu z importovaných dat
+4. Dynamické vytvoření testů
+5. Vytvoření projectName
+6. Vytvoření datumů pro testy
+7. Testovací kroky
+8. Testy spustíme a opravíme případné chyby
+*/
+
---
+++ page-objects/pmtool/projects/project_info_page.js
@@ -0,0 +1,11 @@
+// project_info_page.js
+// cypress/page-objects/pmtool/projects
+
+import { HeaderSection } from "../../common/header_section";
+
+export class projectInfoPage extends HeaderSection {
+    constructor(projectId = ""){
+        super()
+
+    }
+}
